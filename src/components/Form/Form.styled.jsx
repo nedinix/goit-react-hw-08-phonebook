d4b13676b86f05b6c5a2a793307ba9901ed57d06
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-06-08 00:32:51 +0300

    summary

--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -20,6 +20,9 @@ export const StyledFormPhonebook = styled.form`
     border-radius: 4px;
     border: transparent;
 
+    transition: box-shadow 150ms linear, outline 150ms linear,
+      color 100ms linear;
+
     &::placeholder {
       color: rgb(99, 99, 99);
     }
@@ -34,34 +37,35 @@ export const StyledFormPhonebook = styled.form`
     }
   }
 
-  button {
-    display: block;
-    height: 28px;
-    margin-left: auto;
-
-    color: rgb(99, 99, 99);
-
-    border: 1px solid rgb(198, 198, 198);
-    border-radius: 4px;
-
-    transition: box-shadow 150ms linear, background-color 10s0ms linear,
-      color 100ms linear;
-  }
-
-  button:hover,
-  button:focus {
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-  }
-
-  button:active {
-    background-color: rgb(59, 59, 59);
-    color: rgb(248, 248, 248);
-  }
-
   & > div {
     display: flex;
     flex-direction: column;
 
     margin-bottom: 12px;
+  }
+`;
+
+export const StyledFormPhonebookButton = styled.button`
+  display: block;
+  height: 28px;
+  margin-left: auto;
+
+  color: rgb(99, 99, 99);
+
+  border: 1px solid rgb(198, 198, 198);
+  border-radius: 4px;
+
+  transition: box-shadow 150ms linear, background-color 100ms linear,
+    color 100ms linear;
+
+  &:hover,
+  &:focus {
+    background-color: rgb(241, 241, 241);
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  }
+
+  &:active {
+    background-color: rgb(59, 59, 59);
+    color: rgb(248, 248, 248);
   }
 `;
